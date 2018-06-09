ed984ce5354301e048c06c6fef16d515ad61705f
aekt <yusan-23@hotmail.com>
2018-06-09 23:53:19 +0800

    first try on d3js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # scathn
+
+Using [Hacker News API](https://github.com/HackerNews/API) and
+[D3.js](https://d3js.org/).
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let firebase = "https://hacker-news.firebaseio.com";
 
 let newstories = "/v0/newstories.json";
@@ -38,28 +40,91 @@ function makeRow(id) {
     return row;
 }
 
+function run2() {
+    
+    const svgWidth = 600;
+    const svgHeight = 600;
+    
+    const margin = 30;
+    const maxTick = 5;
+
+    const maxScore = 700;
+    const maxComment = 700;
+    const maxStory = 10;
+
+    const scatterSize = 3;
+
+    let svg = d3.select("body")
+        .append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+    
+    let xScale = d3.scaleLinear()
+	.domain([0, maxScore])
+	.range([margin, svgWidth - margin])
+        .nice();
+
+    let yScale = d3.scaleLinear()
+	.domain([0, maxComment])
+	.range([svgHeight - margin, margin])
+        .nice();
+    
+    let xAxis = d3.axisBottom(xScale)
+        .ticks(maxTick);
+
+    let yAxis = d3.axisLeft(yScale)
+        .ticks(maxTick);
+
+    function plotStory(id) {
+        
+        svg.append("g")
+            .attr("transform", `translate(0, ${svgHeight-margin})`)
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("transform", `translate(${margin}, 0)`)
+            .call(yAxis);
+
+        fetch(firebase + getItem(id))
+            .then(resp => resp.json())
+            .then(json => {
+                let x = json["score"];
+                let y = json["descendants"];
+                if (x == undefined || y == undefined) return;
+                svg.append("circle")
+                    .attr("cx", xScale(x))
+		    .attr("cy", yScale(y))
+		    .attr("r", scatterSize);
+            });
+    }
+
+    fetch(firebase + topstories)
+        .then(resp => resp.json())
+        .then(json => {
+            let N = Math.min(maxStory, json.length);
+            for (let i = 0; i < N; i++) {
+                plotStory(json[i]);
+            }
+        });
+}
+
 function run() {
     fetch(firebase + topstories)
 	.then(resp => resp.json())
 	.then(json => {
-            let main = document.querySelector("#main");
-            while (main.firstChild) {
-                main.removeChild(main.firstChild);
-            }
-
 	    let table = document.createElement("table");
 	    let tbody = document.createElement("tbody");
 
-            let maxRows = Math.min(30, json.length);
+            let maxRows = Math.min(10, json.length);
 	    for (let i = 0; i < maxRows; i++) {
 		tbody.appendChild(makeRow(json[i]));
                 fillRow(json[i]);
 	    }
 
 	    table.appendChild(tbody);
-	    main.append(table);
+	    document.body.append(table);
 	});
 }
 
-let button = document.querySelector("button");
-button.addEventListener("click", run, false);
+run();
+run2();
