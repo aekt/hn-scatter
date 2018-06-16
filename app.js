4e4ce8689bee683032fd7866e96026c7383a170b
aekt <yusan-23@hotmail.com>
2018-06-17 02:51:43 +0800

    implemented new display form, added links for each circle, styled visited links

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 "use strict";
 
-const maxScore = 500;
-const maxComment = 500;
-const maxStory = 10;
+// Axes
 
-const margin = {top: 30, right: 30, bottom: 30, left: 30};
-const maxTick = 5;
-const scatterSize = 7;
+let maxScore = 600;
+let maxComment = 600;
+let maxStory = 10;
 
-const svgWidth = 600;
-const svgHeight = 600;
-const width = svgWidth - margin.left - margin.right;
-const height = svgHeight - margin.top - margin.bottom;
+let margin = {top: 30, right: 30, bottom: 30, left: 30};
+let maxTick = 5;
+let maxRadius = 5;
+
+let svgWidth = document.body.clientWidth;
+let svgHeight = document.body.clientHeight;
+let width = Math.min(800, svgWidth - margin.left - margin.right);
+let height = Math.min(800, svgHeight - margin.top - margin.bottom);
+
+margin.left = margin.right = (svgWidth - width) / 2;
+margin.top = margin.bottom = (svgHeight - height) / 2;
 
 let xScale = d3.scaleLinear()
     .domain([0, maxScore])
@@ -29,8 +34,9 @@ let yAxis = d3.axisLeft(yScale)
     .ticks(maxTick)
     .tickSize(-width);
 
-let plot = d3.select("body").append("div");
-plot.attr("align", "center");
+let plot = d3.select("body")
+    .append("div")
+    .attr("align", "center");
 
 let svg = plot.append("svg")
     .attr("width", svgWidth)
@@ -54,6 +60,9 @@ g.selectAll(".tick text")
     .style("fill", "grey");
 
 
+
+// Dots
+
 let firebase = "https://hacker-news.firebaseio.com";
 let newstories = "/v0/newstories.json";
 let topstories = "/v0/topstories.json";
@@ -61,61 +70,93 @@ let beststories = "/v0/beststories.json";
 let getItem = ((id) => "/v0/item/" + id.toString() + ".json");
 
 let items = {};
-let currentItem;
-
-let overItemBox = d3.select("body").append("div");
-let highItemsBox = d3.select("body").append("div");
-
-overItemBox.style("min-height", "3em");
-
-highItemsBox.append("span").text("Over 500 points/comments:");
-highItemsBox.append("br");
-
-function showItem(id, div) {
-
-    let item = items[id];
-    let title = item.title;
-    let url = item.url;
-    let score = item.score;
-    let descendants = item.descendants;
-
-    div.append("span")
-	.text("(" + score + " point" + (score > 1 ? "s" : "") + " | ");
-    div.append("span").append("a")
-	.text(descendants + " comment" + (descendants > 1 ? "s" : ""))
-	.attr("href", "https://news.ycombinator.com/item?id=" + id);
-    div.append("span")
-        .text(") ");
-    div.append("span").append("a")
-	.text(title)
-	.attr("href", url);
-    div.append("br");
-}
 
 function plotItem(id) {
-
-    let x = items[id].score;
-    let y = items[id].descendants;
-
-    if (x == undefined || y == undefined) return;
-
-    if (x > maxScore || y > maxComment) {
-	showItem(id, highItemsBox);
-	return;
+    function plural(number, word) {
+        return number + " " + word + (number > 1 ? "s" : "");
     }
     
-    g.append("circle")
-	.attr("class", `dot${id}`)
-        .attr("cx", xScale(x))
-	.attr("cy", yScale(y))
-	.attr("r", Math.max(2, scatterSize * (x/maxScore + y/maxComment)))
-	.on("mouseover", () => {
-	    if (currentItem != id) {
-		currentItem = id;
-		overItemBox.selectAll("*").remove();
-		showItem(id, overItemBox);
-	    }
-	});
+    function itemTitle(id) {
+        return items[id].title;
+    }
+    
+    function itemPoint(id) {
+        return plural(items[id].score, "point");
+    }
+    
+    function itemComment(id) {
+        return plural(items[id].descendants, "comment");
+    }
+
+    let {score, descendants: comment, url} = items[id];
+    if (score == undefined || comment == undefined) return;
+    
+    score = Math.min(score, maxScore);
+    comment = Math.min(comment, maxComment);
+
+    let x = xScale(score);
+    let y = yScale(comment);
+    let r = Math.max(2, maxRadius * (score/maxScore + comment/maxComment));
+    let offset = r+12;
+    let pad = 5;
+    let radius = 2;
+    
+    let a = g.append("a")
+        .attr("target", "_blank")
+        .attr("href", url);
+    
+    let c = a.append("circle")
+        .attr("cx", x)
+        .attr("cy", y)
+        .attr("r", r);
+
+    c.on("mouseover", () => {
+        let tipGroup = g.append("g")
+            .attr("transform", `translate(${x}, ${y-offset})`)
+            .attr("id", "tipGroup");
+        
+        let tipRect = tipGroup.append("rect")
+            .attr("fill", "#222");
+        
+        let tipBox = tipGroup.append("g")
+            .attr("id", "tipBox");
+        
+        tipBox.append("text")
+            .text("|")
+            .style("text-anchor", "middle");
+        
+        tipBox.append("text")
+            .text(itemTitle(id))
+            .attr("dy", "-1.6em")
+            .style("text-anchor", "middle");
+
+        tipBox.append("text")
+            .text(itemPoint(id))
+            .attr("dx", "-0.618em")
+            .style("text-anchor", "end");
+
+        tipBox.append("text")
+            .text(itemComment(id))
+            .attr("dx", "0.618em")
+            .style("text-anchor", "start");
+        
+        tipBox.selectAll("text")
+            .style("fill", "#fff");
+
+        let bound = document.querySelector("#tipBox").getBBox();
+        tipRect
+            .attr("x", bound.x - pad)
+            .attr("y", bound.y - pad)
+            .attr("rx", radius)
+            .attr("ry", radius)
+            .attr("width", bound.width + 2 * pad)
+            .attr("height", bound.height + 2 * pad);
+    });
+
+    c.on("mouseout", () => {
+        g.select("#tipGroup")
+            .remove();
+    });
 }
 
 function fetchItem(id) {
@@ -131,7 +172,7 @@ function run() {
     fetch(firebase + topstories)
         .then(resp => resp.json())
         .then(json => {
-            let maxStory = Math.min(100, json.length);
+            let maxStory = Math.min(90, json.length);
             for (let i = 0; i < maxStory; i++) {
                 fetchItem(json[i]);
             }
